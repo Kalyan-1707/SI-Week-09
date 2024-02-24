7b9721cd681931893f8493360550b0ad6338668b
Pavan Kalyan Konudula <52063829+Kalyan-1707@users.noreply.github.com>
2024-02-24 12:24:55 -0500

    Update sayName.mjs

--- functions/sayName.mjs
+++ functions/sayName.mjs
@@ -3,5 +3,5 @@ export default async (req, context) => {
     const searchParams = new URLSearchParams(req.url.split('?')[1]);
     const name = searchParams.get('name');
 
-    return new Response(`Hello ${name}!`);
+    return new Response(JSON.stringify({message: `${name} says hello!`}));
 }
