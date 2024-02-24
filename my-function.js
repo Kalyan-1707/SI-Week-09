9958cc86739e5250eb0a4ce58b4afb391c2ae988
Pavan Kalyan Konudula <52063829+Kalyan-1707@users.noreply.github.com>
2024-02-24 12:37:46 -0500

    Create my-function.js

---
+++ my-function.js
@@ -0,0 +1,7 @@
+export default async (req, context) => {
+
+    const searchParams = new URLSearchParams(req.url.split('?')[1]);
+    const name = searchParams.get('name');
+
+    return new Response(JSON.stringify({message: `${name} says hello!`}));
+}
